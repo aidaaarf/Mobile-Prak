551d8b951ccbdcd32bcfcf8e26aca3dd540f14cd
aidas777-ctrl <fadhila2903@gmail.com>
2025-09-10 12:09:44 +0700

    Langkah 1 dan 2 (praktikum1)

---
+++ Bagian3/praktikum1.dart
@@ -0,0 +1,15 @@
+// Ketik atau salin kode program berikut ke dalam void main().
+
+var list = [1, 2, 3];
+assert(list.length == 3);
+assert(list[1] == 2);
+print(list.length);
+print(list[1]);
+
+list[1] = 1;
+assert(list[1] == 1);
+print(list[1]);
+
+// Silakan coba eksekusi (Run) kode pada langkah 1 tersebut. Apa yang terjadi? Jelaskan!
+
+// Ketika di run code error karena kode tidak ada di dalam fungsi main(). Sehingga assert mungkin di-disable.
