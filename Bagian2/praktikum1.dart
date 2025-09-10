aa5bad270eed4bc10c6557abd0702a9d11b46787
aidas777-ctrl <fadhila2903@gmail.com>
2025-09-10 11:49:18 +0700

    Langkah 3

--- Bagian2/praktikum1.dart
+++ Bagian2/praktikum1.dart
@@ -1,15 +1,20 @@
-// Ketik atau salin kode program berikut ke dalam fungsi main().
-String test = "test2";
-if (test == "test1") {
-   print("Test1");
-} else If (test == "test2") {
-   print("Test2");
-} Else {
-   print("Something else");
+void main() {
+  String test = "test2";
+
+  if (test == "test1") {
+    print("Test1");
+  } else if (test == "test2") {
+    print("Test2");
+  } else {
+    print("Hallo");
+  }
+
+  if (test == "test2") {
+    print("Anyeong");
+  }
+
+  String strTest = "true";
+  if (strTest == "true") {
+    print("true min");
+  }
 }
-if (test == "test2") print("Test2 again");
-
-// Silakan coba eksekusi (Run) kode pada langkah 1 tersebut. Apa yang terjadi? Jelaskan!
-
-// Ketika dieksekusi, kode mengalami error karena Dart merupakan case-sensitive sehingga 
-// ketika menuliskan else dengan huruf besar (Else) maka program tidak akan mengenali. 
