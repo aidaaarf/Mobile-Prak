06b9e01ad91d4d2014c6d4a88cb3c991928de729
aidas777-ctrl <fadhila2903@gmail.com>
2025-09-10 12:11:29 +0700

    Langkah 3 (praktikum1)

--- Bagian3/praktikum1.dart
+++ Bagian3/praktikum1.dart
@@ -1,15 +1,10 @@
-// Ketik atau salin kode program berikut ke dalam void main().
+// Ubah kode pada langkah 1 menjadi variabel final yang mempunyai indeks = 5 dengan default value = null. Isilah nama dan NIM Anda pada elemen index ke-1 dan ke-2. Lalu print dan capture hasilnya.
 
-var list = [1, 2, 3];
-assert(list.length == 3);
-assert(list[1] == 2);
-print(list.length);
-print(list[1]);
+void main() {
+  final List<dynamic?> list = List.filled(5, null);
 
-list[1] = 1;
-assert(list[1] == 1);
-print(list[1]);
+  list[1] = "Aida Rahma Fadhila";
+  list[2] = "2341720094";
 
-// Silakan coba eksekusi (Run) kode pada langkah 1 tersebut. Apa yang terjadi? Jelaskan!
-
-// Ketika di run code error karena kode tidak ada di dalam fungsi main(). Sehingga assert mungkin di-disable.
+  print(list);
+}
