57622f54020baf0d4c5d88c880a06b9fcf13c900
aidas777-ctrl <fadhila2903@gmail.com>
2025-09-10 15:02:55 +0700

    Langkah 3 (praktikum3)

--- Bagian3/praktikum3.dart
+++ Bagian3/praktikum3.dart
@@ -1,21 +1,37 @@
-// Ketik atau salin kode program berikut ke dalam fungsi main().
+void main() {
 
-var gifts = {
-  // Key:    Value
-  'first': 'partridge',
-  'second': 'turtledoves',
-  'fifth': 1
-};
+  var gifts = <String, String>{
+    'first': 'partridge',
+    'second': 'turtledoves',
+    'fifth': 'golden rings',
+    'name': 'Aida Rahma Fadhila',   
+    'nim': '2341720094'     
+  };
 
-var nobleGases = {
-  2: 'helium',
-  10: 'neon',
-  18: 2,
-};
+  var nobleGases = <int, String>{
+    2: 'helium',
+    10: 'neon',
+    18: 'argon',
+    99: 'Aida Rahma Fadhila',   
+    100: '2341720094'    
+  };
 
-print(gifts);
-print(nobleGases);
+  var mhs1 = Map<String, String>();
+  mhs1['first'] = 'partridge';
+  mhs1['second'] = 'turtledoves';
+  mhs1['fifth'] = 'golden rings';
+  mhs1['name'] = 'Aida Rahma Fadhila';   
+  mhs1['nim'] = '2341720094';     
 
-// Silakan coba eksekusi (Run) kode pada langkah 1 tersebut. Apa yang terjadi? Jelaskan! Lalu perbaiki jika terjadi error.
-
-// karena key memiliki type yang tidak konsisten, maka ganti map dengan tipe dynamic supaya semua value bisa bercampur
+  var mhs2 = Map<int, String>();
+  mhs2[2] = 'helium';
+  mhs2[10] = 'neon';
+  mhs2[18] = 'argon';
+  mhs2[99] = 'Aida Rahma Fadhila';   
+  mhs2[100] = '2341720094';   
+  
+  print("gifts: $gifts");
+  print("nobleGases: $nobleGases");
+  print("mhs1: $mhs1");
+  print("mhs2: $mhs2");
+}
