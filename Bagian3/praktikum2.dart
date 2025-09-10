6ccdc5aeac1c0e101ba8bf23068048a4388a1e64
aidas777-ctrl <fadhila2903@gmail.com>
2025-09-10 12:13:10 +0700

    Langkah 1 dan 2 (praktikum2)

---
+++ Bagian3/praktikum2.dart
@@ -0,0 +1,7 @@
+// Ketik atau salin kode program berikut ke dalam fungsi main().
+var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+print(halogens);
+
+// Silakan coba eksekusi (Run) kode pada langkah 1 tersebut. Apa yang terjadi? Jelaskan! Lalu perbaiki jika terjadi error.
+
+// Ketika di run code error karena kode tidak ada di dalam fungsi main(). 
