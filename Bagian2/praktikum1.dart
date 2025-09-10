658f0167018e8b4de45a059e08368f07d392f2c9
aidas777-ctrl <fadhila2903@gmail.com>
2025-09-10 11:46:46 +0700

    Langkah 2

--- Bagian2/praktikum1.dart
+++ Bagian2/praktikum1.dart
@@ -8,3 +8,8 @@ if (test == "test1") {
    print("Something else");
 }
 if (test == "test2") print("Test2 again");
+
+// Silakan coba eksekusi (Run) kode pada langkah 1 tersebut. Apa yang terjadi? Jelaskan!
+
+// Ketika dieksekusi, kode mengalami error karena Dart merupakan case-sensitive sehingga 
+// ketika menuliskan else dengan huruf besar (Else) maka program tidak akan mengenali. 
