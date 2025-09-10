9c00c093aa0bf45de4f38abaa6f0f05301af72b6
aidas777-ctrl <fadhila2903@gmail.com>
2025-09-10 11:55:18 +0700

    Langkah 1 dan 2

---
+++ Bagian2/praktikum3.dart
@@ -0,0 +1,10 @@
+// Ketik atau salin kode program berikut ke dalam fungsi main()
+for (Index = 10; index < 27; index) {
+  print(Index);
+}
+
+// Silakan coba eksekusi (Run) kode pada langkah 1 tersebut. Apa yang terjadi? Jelaskan! Lalu perbaiki jika terjadi error.
+
+// Terjadi error saat dieksekusi (run) karena variabel index tidak dideklarasikan, dart merupakan 
+// case-sensitive sehingga penulisan harus konsisten (menggunakan huruf kecil), update counter 
+// salah seharusnya index++
