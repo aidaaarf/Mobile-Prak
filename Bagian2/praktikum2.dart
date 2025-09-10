def29c9d1fb280f4f2e25982ab60e8afa45394fc
aidas777-ctrl <fadhila2903@gmail.com>
2025-09-10 11:51:17 +0700

    Langkah 1 dan 2

---
+++ Bagian2/praktikum2.dart
@@ -0,0 +1,10 @@
+// Ketik atau salin kode program berikut ke dalam fungsi main().
+while (counter < 33) {
+  print(counter);
+  counter++;
+}
+
+// Silakan coba eksekusi (Run) kode pada langkah 1 tersebut. Apa yang terjadi? Jelaskan! Lalu perbaiki jika terjadi error.
+
+// Terjadi error saat dieksekusi (run) karena variabel counter belum dideklarasikan sebelum 
+// dipakai di while. Sehingga harus diinisialisasi terlebih dahulu 
