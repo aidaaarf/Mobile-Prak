a45c66a28bcfe0bc6896b86c624771378b9fbc05
aidas777-ctrl <fadhila2903@gmail.com>
2025-09-10 14:49:12 +0700

    Langkah 1 dan 2 (praktikum3)

---
+++ Bagian3/praktikum3.dart
@@ -0,0 +1,21 @@
+// Ketik atau salin kode program berikut ke dalam fungsi main().
+
+var gifts = {
+  // Key:    Value
+  'first': 'partridge',
+  'second': 'turtledoves',
+  'fifth': 1
+};
+
+var nobleGases = {
+  2: 'helium',
+  10: 'neon',
+  18: 2,
+};
+
+print(gifts);
+print(nobleGases);
+
+// Silakan coba eksekusi (Run) kode pada langkah 1 tersebut. Apa yang terjadi? Jelaskan! Lalu perbaiki jika terjadi error.
+
+// karena key memiliki type yang tidak konsisten, maka ganti map dengan tipe dynamic supaya semua value bisa bercampur
