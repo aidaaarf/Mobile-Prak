cc4287cbe200b39a02b8d0f7b0594e1266315c57
aidas777-ctrl <fadhila2903@gmail.com>
2025-09-10 12:16:09 +0700

    Langkah 3 (praktikum2)

--- Bagian3/praktikum2.dart
+++ Bagian3/praktikum2.dart
@@ -1,7 +1,19 @@
-// Ketik atau salin kode program berikut ke dalam fungsi main().
-var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
-print(halogens);
+// Tambahkan kode program berikut, lalu coba eksekusi (Run) kode Anda.
+// Apa yang terjadi ? Jika terjadi error, silakan perbaiki namun tetap menggunakan ketiga variabel tersebut.
+//Tambahkan elemen nama dan NIM Anda pada kedua variabel Set tersebut dengan dua fungsi berbeda yaitu .add() dan .addAll().
+//Untuk variabel Map dihapus, nanti kita coba di praktikum selanjutnya.
 
-// Silakan coba eksekusi (Run) kode pada langkah 1 tersebut. Apa yang terjadi? Jelaskan! Lalu perbaiki jika terjadi error.
+void main() {
+  var names1 = <String>{};
+  Set<String> names2 = {};
+  var names3 = {};
 
-// Ketika di run code error karena kode tidak ada di dalam fungsi main(). 
+  names1.add("Aida Rahma Fadhila");
+  names1.add("2341720094");
+
+  names2.addAll({"Aida Rahma", "123456789"});
+
+  print("names1 (Set): $names1");
+  print("names2 (Set): $names2");
+  print("names3 (Map): $names3");
+}
