4741237aac8f5c4604f70a92c4c646fa2e12731f
aidas777-ctrl <fadhila2903@gmail.com>
2025-09-11 12:08:12 +0700

    langkah 6 (praktikum4)

--- Bagian3/praktikum4.dart
+++ Bagian3/praktikum4.dart
@@ -4,23 +4,23 @@ void main() {
   var list = [1, 2, 3];
   var list2 = [0, ...list];
 
-  print(list);         
-  print(list2);         
-  print(list2.length);  
+  print(list);
+  print(list2);
+  print(list2.length);
   var list1 = [1, 2, null];
-  print(list1);         
+  print(list1);
 
   var list3 = [0, ...?list1];
-  print(list3);         
-  print(list3.length); 
+  print(list3);
+  print(list3.length);
 
-  var nimList = ['2', '3', '4', '1', '7', '2', '0','0', '9','4'];
+  var nimList = ['2', '3', '4', '1', '7', '2', '0', '0', '9', '4'];
   var fullList = [...list3, ...nimList];
 
-  print(nimList); 
-  print(fullList);  
+  print(nimList);
+  print(fullList);
 
-    // Jika promoActive = true
+  // Jika promoActive = true
   var promoActive = true;
   var nav = ['Home', 'Furniture', 'Plants', if (promoActive) 'Outlet'];
   print("promoActive = true -> $nav");
@@ -31,10 +31,20 @@ void main() {
   print("promoActive = false -> $nav");
 
   var login = 'Manager';
-  var nav2 = ['Home', 'Furniture', 'Plants', if (login case 'Manager') 'Inventory'];
+  var nav2 = [
+    'Home',
+    'Furniture',
+    'Plants',
+    if (login case 'Manager') 'Inventory',
+  ];
   print("login = Manager -> $nav2");
 
   login = 'Admin';
   nav2 = ['Home', 'Furniture', 'Plants', if (login case 'Manager') 'Inventory'];
   print("login = Admin -> $nav2");
+
+  var listOfInts = [1, 2, 3];
+  var listOfStrings = ['#0', for (var i in listOfInts) '#$i'];
+  assert(listOfStrings[1] == '#1');
+  print(listOfStrings);
 }
