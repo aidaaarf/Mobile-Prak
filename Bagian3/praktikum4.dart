466459bc3cfd78e7c8716076ba30d585d7830cda
aidas777-ctrl <fadhila2903@gmail.com>
2025-09-11 11:28:25 +0700

    Langkah 1 dan 2 (praktikum4)

---
+++ Bagian3/praktikum4.dart
@@ -0,0 +1,10 @@
+// Ketik atau salin kode program berikut ke dalam fungsi main().
+var list = [1, 2, 3];
+var list2 = [0, ...list];
+print(list1);
+print(list2);
+print(list2.length);
+
+// Silakan coba eksekusi (Run) kode pada langkah 1 tersebut. Apa yang terjadi? Jelaskan! Lalu perbaiki jika terjadi error.
+// error
+// karena variabel list1 tidak ada / belum di deklarasikan
