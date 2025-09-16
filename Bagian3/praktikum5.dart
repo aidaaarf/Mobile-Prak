90a6579fe781209cb9cf3da5893c88729798a4a6
aidas777-ctrl <fadhila2903@gmail.com>
2025-09-16 20:18:31 +0700

    langkah 3 (praktikum5)

--- Bagian3/praktikum5.dart
+++ Bagian3/praktikum5.dart
@@ -1,14 +1,14 @@
-// Ketik atau salin kode program berikut ke dalam fungsi main().
-
-// var record = ('first', a: 2, b: true, 'last');
-// print(record)
-
-// Silakan coba eksekusi (Run) kode pada langkah 1 tersebut. Apa yang terjadi? Jelaskan! Lalu perbaiki jika terjadi error.
-// error karena fields dengan nama harus ditulis setelah positional fields, tidak boleh ada positional fields setelahnya.
-
-// perbaikan
 void main() {
+  (String, String, {int a, bool b}) tukar((String, String, {int a, bool b}) record) {
+    var (x, y, :a, :b) = record;
+    return (y, x, a: a, b: b);
+  }
+
   var record = ('first', 'last', a: 2, b: true);
-  print(record.a);
-  print(record.b);
+  print("Sebelum tukar: $record");
+
+  var hasil = tukar(record);
+  print("Sesudah tukar: $hasil");
 }
+
+
