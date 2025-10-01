d4400e6bc9632cacacca2d192fa994ed04e0359f
aidas777-ctrl <fadhila2903@gmail.com>
2025-10-01 10:58:56 +0700

    langkah 4 (praktikum 1)

--- Bagian5/layout_flutter/lib/main.dart
+++ Bagian5/layout_flutter/lib/main.dart
@@ -4,6 +4,47 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
+  Widget get titleSection => Container(
+    // soal 3: Seluruh baris ada di dalam Container dan beri padding di sepanjang setiap tepinya sebesar 32 piksel.
+    padding: const EdgeInsets.all(32.0),
+    child: Row(
+      children: [
+        Expanded(
+          /* soal 1: Letakkan widget Column di dalam widget Expanded */
+          child: Column(
+            // soal 1: Tambahkan properti crossAxisAlignment ke CrossAxisAlignment.start
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /* soal 2: Letakkan baris pertama teks di dalam Container sehingga memungkinkan Anda untuk menambahkan padding = 8. */
+              Container(
+                padding: const EdgeInsets.only(bottom: 8.0), // padding = 8
+                child: const Text(
+                  'Wisata Gunung di Batu',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                'Batu, Malang, Indonesia',
+                style: const TextStyle(
+                  // soal 2: set warna menjadi abu-abu
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+        /* soal 3: Dua item terakhir di baris judul adalah ikon bintang, set dengan warna merah, dan teks "41". */
+        Icon(
+          Icons.star, // Ikon bintang
+          color: Colors.red[500], // set dengan warna merah
+        ),
+        const Text('41'), // teks "41"
+      ],
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -13,8 +54,11 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        // Ganti isi body text 'Hello World' dengan variabel titleSection
+        body: Column(
+          children: [
+            titleSection,
+          ],
         ),
       ),
     );
